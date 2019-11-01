939dde87a25fa0fda30c910eda08ddc53988971e
georgehadjiantonis@hotmail.com <georgehadjiantonis@hotmail.com>
2019-11-01 20:30:04 +0200

    add data preprocessing, parameter files, startes defining attributes and classes to KohonenNN and KohonenNNDriver

--- src/io/github/ghadj/kohonenneuralnetwork/KohonenNN.java
+++ src/io/github/ghadj/kohonenneuralnetwork/KohonenNN.java
@@ -1,5 +1,56 @@
 package io.github.ghadj.kohonenneuralnetwork;
 
-public class KohonenNN {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
+public class KohonenNN {
+    private double[][][] weights;
+    private double[][] distance;
+    private char[][] labels;
+    private int winnerX = 0;
+    private int winnerY = 0;
+    private double learningRate;
+    private ArrayList<Double> trainErrorList = new ArrayList<Double>();
+    private ArrayList<Double> testErrorList = new ArrayList<Double>();
+
+    public KohonenNN(int gridSize, double learningRate, int dataDimension) {
+        this.weights = new double[gridSize][gridSize][dataDimension];
+        for (int i = 0; i < gridSize; i++)
+            for (int j = 0; j < gridSize; j++)
+                for (int k = 0; k < dataDimension; k++)
+                    this.weights[i][j][k] = (new Random()).nextDouble() - 0.5;
+        this.distance = new double[gridSize][gridSize];
+        this.labels = new char[gridSize][gridSize];
+        this.learningRate = learningRate;
+    }
+
+    public void run(Map<Character, List<Double>> data, Boolean train){
+
+    }
+
+    /**
+     * Returns a list containing the mean of the squared error per epoch, during
+     * training.
+     * 
+     * @return list containing the mean of the squared error per epoch.
+     */
+    public ArrayList<Double> getTrainErrorList() {
+        return trainErrorList;
+    }
+
+    /**
+     * Returns a list containing the mean of the squared error per epoch, during
+     * test.
+     * 
+     * @return list containing the mean of the squared error per epoch.
+     */
+    public ArrayList<Double> getTestErrorList() {
+        return testErrorList;
+    }
+
+    public char[][] getLabels() {
+        return labels;
+    }
 }
--- src/io/github/ghadj/kohonenneuralnetwork/KohonenNNDriver.java
+++ src/io/github/ghadj/kohonenneuralnetwork/KohonenNNDriver.java
@@ -1,5 +1,177 @@
 package io.github.ghadj.kohonenneuralnetwork;
 
-public class KohonenNNDriver {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+/**
+ * Driver of the neural network. Takes as input from the arguments the path to
+ * the file, containing the parameters of the neural network. Traings the neural
+ * network based on the given parameters and exports the error and success rate
+ * per epoch in two separate files.
+ * 
+ * Assume that the parameter file has the following format: 
+ * gridSize 80
+ * learningRate 0.3 
+ * maxIterations 10000 
+ * dataDimension 16
+ * trainFile ../Parameters/training.csv
+ * testFile ../Parameters/test.csv
+ * 
+ *  Compile from Simple-Neural-Network/ directory
+ * javac -d ./bin ./src/io/github/ghadj/simpleneuralnetwork/*.java
+ * 
+ * Run from Simple-Neural-Network/ directory java -cp ./bin
+ * io.github.ghadj.simpleneuralnetwork.SimpleNeuralNetworkDriver <path to
+ * parameters.txt>
+ * 
+ * @author Georgios Hadjiantonis
+ * @since 01-11-2019
+ */
+public class  KohonenNNDriver{
+    private static final String errorFilename = "errors.txt";
+
+    /**
+     * Reads the parameters of the neural network from the given file.
+     * 
+     * @param filename path to the file containing the parameters
+     * @return a String array containing the parameters.
+     * @throws FileNotFoundException
+     * @throws IOException
+     * @throws InvalidParameterException
+     */
+    public static String[] readParameters(String filename)
+            throws FileNotFoundException, IOException, InvalidParameterException {
+        File file = new File(filename);
+        BufferedReader br;
+        String[] parameters = new String[9];
+        int i = 0;
+
+        br = new BufferedReader(new FileReader(file));
+        String st;
+        while ((st = br.readLine()) != null)
+            parameters[i++] = st.split(" ")[1];
+        br.close();
+
+        if (i != 9)
+            throw new InvalidParameterException("Invalid parameters given.");
+        return parameters;
+    }
+
+    /**
+     * Reads data from the given file. Returns a map in the form of <input list,
+     * output list>.
+     * 
+     * @param numInputNeurons  number of input neurons.
+     * @param numOutputNeurons number of output neurons.
+     * @param filename         name of file to be read.
+     * @return a map in the form of <input list, output list>.
+     * @throws FileNotFoundException
+     * @throws IOException
+     * @throws InvalidParameterException in case the data of the given file is
+     *                                   inconsistent.
+     */
+    public static Map<Character, List<Double>> readData(int numInputNeurons, int numOutputNeurons, String filename)
+            throws FileNotFoundException, IOException, InvalidParameterException {
+        Map<Character, List<Double>> data = new HashMap<Character, List<Double>>();
+        File file = new File(filename);
+        BufferedReader br;
+        br = new BufferedReader(new FileReader(file));
+        String st;
+        while ((st = br.readLine()) != null) {
+            List<Double> input = new ArrayList<>();
+            List<Double> output = new ArrayList<>();
+            int i = 0;
+            String[] line = st.split(",");
+            if (line.length != numInputNeurons + numOutputNeurons) {
+                br.close();
+                throw new InvalidParameterException("Inconsistent data given in file " + filename);
+            }
+
+            for (int j = 0; j < numInputNeurons; j++)
+                input.add(Double.parseDouble(line[i++]));
+            for (int j = 0; j < numOutputNeurons; j++)
+                output.add(Double.parseDouble(line[i++]));
+
+            data.put(input, output);
+        }
+        br.close();
+        return data;
+    }
+
+    /**
+     * Runs the NN based on the parameters, training and testing given data. Writes
+     * the squares error and success rate to two separate files at the end of all
+     * the iterations.
+     * 
+     * @param parameters
+     * @param trainingData
+     * @param testData
+     * @throws IOException
+     */
+    public static void run(String[] parameters, Map<Character, List<Double>> trainingData,
+            Map<Character, List<Double>> testData) throws IOException {
+
+        KohonenNN nn = new KohonenNN(Integer.parseInt(parameters[0]), Double.parseDouble(parameters[1]),Integer.parseInt(parameters[0]));
+        for (int i = 0; i < Integer.parseInt(parameters[6]); i++) {
+            nn.run(trainingData, true);
+            nn.run(testData, false);
+        }
+        List<Double> trainError = nn.getTrainErrorList();
+        List<Double> testError = nn.getTestErrorList();
+        writeResults(trainError, testError, errorFilename);
+    }
+
+    /**
+     * Writes the results in csv format to the file given.
+     * 
+     * @param trainResults
+     * @param testResults
+     * @param filename
+     * @throws IOException
+     */
+    public static void writeResults(List<Double> trainResults, List<Double> testResults, String filename)
+            throws IOException {
+        Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filename), "utf-8"));
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < trainResults.size() && i < testResults.size(); i++)
+            str.append((i + 1) + "," + trainResults.get(i) + "," + testResults.get(i) + "\n");
+        writer.write(str.toString());
+        writer.close();
+    }
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Error: Enter the path to the parameters.txt as an argument to the program.");
+            return;
+        }
+        Map<List<Double>, List<Double>> trainingData, testData;
+        String[] parameters;
+        try {
+            parameters = readParameters(args[0]);
+            trainingData = readData(Integer.parseInt(parameters[2]), Integer.parseInt(parameters[3]), parameters[7]);
+            testData = readData(Integer.parseInt(parameters[2]), Integer.parseInt(parameters[3]), parameters[8]);
+
+            run(parameters, trainingData, testData);
+        } catch (InvalidParameterException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+    }
 }
